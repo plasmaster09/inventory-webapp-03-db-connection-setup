52408851a48d270025c7c8901511f207d9133131
Matthew Wang <matwan@bergen.org>
2022-02-13 21:30:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Part 03: Database Layer: Schema set-up and first queries
+
+This tutorial follows after:
+[Part 02: Web app server setup and basics](https://github.com/atcs-wang/inventory-webapp-02-app-server-basics)
+
+Technologies: [MySQL](https://nodejs.org/en/), [NodeJS mysql library](https://www.npmjs.com/package/mysql)
+
+The third layer of a web app is the **database**. In this tutorial, we will set up a schema for the data of our web app, and write some standard "CRUD" queries. Later, this schema will be improved and expanded.
+
---
+++ app.js
@@ -0,0 +1,28 @@
+//set up the server
+const express = require( "express" );
+const logger = require("morgan");
+const app = express();
+const port = 8080;
+
+// define middleware that logs all incoming requests
+app.use(logger("dev"));
+
+// define a route for the default home page
+app.get( "/", ( req, res ) => {
+    res.sendFile( __dirname + "/views/index.html" );
+} );
+
+// define a route for the stuff inventory page
+app.get( "/stuff", ( req, res ) => {
+    res.sendFile( __dirname + "/views/stuff.html" );
+} );
+
+// define a route for the item detail page
+app.get( "/stuff/item", ( req, res ) => {
+    res.sendFile( __dirname + "/views/item.html" );
+} );
+
+// start the server
+app.listen( port, () => {
+    console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
+} );
