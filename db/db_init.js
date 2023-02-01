44ab4a1140c6048a3fb4575a515026d37ac4bc04
plasmaster09 <spencerschrager@mac.com>
2023-02-01 14:35:13 +0000

    bazingatron 5000

--- db/db_init.js
+++ db/db_init.js
@@ -1,33 +1,23 @@
+const fs = require("fs");
+
 // (Re)Sets up the database, including a little bit of sample data
 const db = require("./db_connection");
 
-/**** Drop existing table, if any ****/
+/**** Delete existing table, if any ****/
 
-const drop_stuff_table_sql = "DROP TABLE IF EXISTS stuff;"
+const drop_stuff_table_sql = fs.readFileSync(__dirname + "/queries/init/drop_stuff_table.sql", { encoding: "UTF-8" });
 
 db.execute(drop_stuff_table_sql);
 
 /**** Create "stuff" table (again)  ****/
 
-const create_stuff_table_sql = `
-    CREATE TABLE stuff (
-        id INT NOT NULL AUTO_INCREMENT,
-        item VARCHAR(45) NOT NULL,
-        quantity INT NOT NULL,
-        description VARCHAR(150) NULL,
-        PRIMARY KEY (id)
-    );
-`
+const create_stuff_table_sql = fs.readFileSync(__dirname + "/queries/init/create_stuff_table.sql", { encoding: "UTF-8" });
+
 db.execute(create_stuff_table_sql);
 
 /**** Create some sample items ****/
 
-const insert_stuff_table_sql = `
-    INSERT INTO stuff 
-        (item, quantity, description) 
-    VALUES 
-        (?, ?, ?);
-`
+const insert_stuff_table_sql = fs.readFileSync(__dirname + "/queries/init/insert_stuff_table.sql", { encoding: "UTF-8" });
 
 db.execute(insert_stuff_table_sql, ['Widgets', '5', 'Widgets are cool! You can do ... so many... different things... with them...']);
 
@@ -35,11 +25,11 @@ db.execute(insert_stuff_table_sql, ['Gizmos', '100', null]);
 
 db.execute(insert_stuff_table_sql, ['Thingamajig', '12345', 'Not to be confused with a Thingamabob']);
 
-db.execute(insert_stuff_table_sql, ['Thingamabob', '54321', 'Not to be confused with a Thingamajig']);
+db.execute(insert_stuff_table_sql, ['BEANS.', '9999999', 'KIRBY. THAT IS ALL YOU NEED TO KNOW.']);
 
 /**** Read the sample items inserted ****/
 
-const read_stuff_table_sql = "SELECT * FROM stuff";
+const read_stuff_table_sql = fs.readFileSync(__dirname + "/queries/init/read_stuff_table.sql", { encoding: "UTF-8" });
 
 db.execute(read_stuff_table_sql, 
     (error, results) => {
@@ -52,17 +42,3 @@ db.execute(read_stuff_table_sql,
 );
 
 db.end();
-
-/*
-
-// Alternatively, instead of putting SQL in string literals, read the SQL from files using the "fs" package.
-// Put this code at the top, and remove all the SQL string literals defined through the file.
-const fs = require("fs");
-
-const drop_stuff_table_sql = fs.readFileSync(__dirname + "/db/queries/init/drop_stuff_table.sql", { encoding: "UTF-8" });
-const create_stuff_table_sql = fs.readFileSync(__dirname + "/db/queries/init/create_stuff_table.sql", { encoding: "UTF-8" });
-const insert_stuff_table_sql = fs.readFileSync(__dirname + "/db/queries/init/insert_stuff_table.sql", { encoding: "UTF-8" });
-const read_stuff_table_sql = fs.readFileSync(__dirname + "/db/queries/init/read_stuff_table.sql", { encoding: "UTF-8" });
-
-*/
-
